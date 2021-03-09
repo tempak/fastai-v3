392615282d80190bb7af5fd4c60d71b3d44c8ee6
Akanksh A Manjunath <amakanksh@gmail.com>
2021-03-09 15:10:08 +0530

    dl=1 for direct download for dropbox files

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/2haxffenzakj9e7/export.pkl?dl=0'
+export_file_url = 'https://www.dropbox.com/s/2haxffenzakj9e7/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['100', '200', '50','500']
